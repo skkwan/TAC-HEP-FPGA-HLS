99d3b69dc5e775e93044c6f607240c249f07591c
Varun Sharma <varun.sharma@cern.ch>
2023-04-11 03:11:32 -0500

    restructuring directories

---
+++ week3/lec5Ex1.h
@@ -0,0 +1,11 @@
+#ifndef LEC5EX1_H_
+#define LEC5EX1_H_
+#define N	11
+
+void lec5Ex1 (
+  int *y,
+  int c[N+1],
+  int x
+  );
+
+#endif
---
+++ week3/lec6Ex2.h
@@ -0,0 +1,21 @@
+#ifndef LEC6EX2_H_
+#define LEC6EX2_H_
+
+#include <stdio.h>
+#include <math.h>
+
+#define NN  30
+#define MM  3
+#define KK  90
+
+void lec6Ex2(
+  unsigned int in[NN],
+  char a,
+  char b,
+  unsigned int c,
+  unsigned int out[KK]
+  );
+
+unsigned int squared(unsigned int );
+
+#endif
