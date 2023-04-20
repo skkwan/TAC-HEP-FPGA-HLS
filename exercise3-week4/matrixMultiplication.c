0c417772c1f41816fab2b916eeb867faf7596c44
Stephanie Kwan <skwan@princeton.edu>
2023-04-20 05:17:21 -0500

    add array_partition pragmas

--- exercise3-week4/matrixMultiplication.c
+++ exercise3-week4/matrixMultiplication.c
@@ -4,6 +4,9 @@ void matrixMultiplication(int matrix1[N], int matrix2[N], int *result) {
 
     int sum = 0;
 
+    #pragma HLS array_partition variable=matrix1 
+    #pragma HLS array_partition variable=matrix2
+
     loop: for (int i = 0; i < N; i++) {
         #pragma HLS unroll
         sum += (matrix1[i] * matrix2[i]);
