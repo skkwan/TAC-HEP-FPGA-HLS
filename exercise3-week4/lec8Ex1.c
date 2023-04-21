95dc644cd8f7a70700991b325d18dbb877643e5e
Stephanie Kwan <skwan@princeton.edu>
2023-04-21 05:41:24 -0500

    add array partition

--- exercise3-week4/lec8Ex1.c
+++ exercise3-week4/lec8Ex1.c
@@ -8,11 +8,13 @@ void lec8Ex1 (
   unsigned int out[N]
   ) {
 
+#pragma HLS ARRAY_PARTITION variable=in block factor=3 dim=1
+#pragma HLS ARRAY_PARTITION variable=out block factor=3 dim=1
+
    unsigned int x, y;
    unsigned int tmp1, tmp2, tmp3;
 
 for_Loop: for (unsigned int i=0 ; i < N; i++) {
-#pragma HLS latency min=4
 	x = in[i];
 	tmp1 = func(1, 2);
 	tmp2 = func(2, 3);
@@ -32,7 +34,6 @@ unsigned int squared(unsigned int a)
 }
 
 unsigned int func(short a, short b){
-
   unsigned int res;
   res= a*a;
   res= res*b*a;
